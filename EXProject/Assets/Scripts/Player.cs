eba1645d74e59944aed1e61c0faf0c9a34d74d24
shigen <arufag4455@gmail.com>
2019-12-14 15:55:09 +0900

    playerの移動

---
+++ EXProject/Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Vector3 pos;
+    public float speed;
+    Rigidbody rd;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pos = transform.position;
+        rd = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+
+    }
+
+    void Move()
+    {
+        float x = Input.GetAxis("Horizontal") * speed;
+        rd.velocity = new Vector3(x, 0, 0);
+    }
+}

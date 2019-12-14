c17834ecb47a90f849eca7c70991fb50ba07a9af
shigen <arufag4455@gmail.com>
2019-12-14 15:55:56 +0900

    ボールの跳ね返り実装

---
+++ EXProject/Assets/Scripts/Boll.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boll : MonoBehaviour
+{
+    public Player player;
+    Vector3 startpos;
+    Rigidbody rd;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startpos = new Vector3(player.transform.position.x, player.transform.position.y + 5, 0);
+        rd = GetComponent<Rigidbody>();
+        rd.AddForce((transform.up + transform.right) * speed, ForceMode.VelocityChange);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.y<=2)
+        {
+            transform.position = startpos;
+        }
+    }
+}
